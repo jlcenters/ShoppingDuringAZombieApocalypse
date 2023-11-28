6f1b2b1969c164534b3bbc9678a406e3d586d2df
Jill Centers <58017334+jlcenters@users.noreply.github.com>
2023-11-28 16:05:32 -0500

    first commit

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private Button startGameButton;
+    [SerializeField] private Button SettingsButton;
+    [SerializeField] private Button QuitGameButton;
+
+
+
+    private void Awake()
+    {
+        
+    }
+    private void OpenSettingsMenu()
+    {
+
+    }
+    private voi
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
